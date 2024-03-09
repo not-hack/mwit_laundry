ac9cfbe0164a84428228e5574aa673935f363108
not-hack <70331517+not-hack@users.noreply.github.com>
2024-03-09 14:50:19 +0700

    Update checkLogin.php

--- server/security/checkLogin.php
+++ server/security/checkLogin.php
@@ -1,12 +1,13 @@
 <?php
     function checkLogin() {
         if (!isset($_SESSION['ID']) OR !isset($_SESSION['class'])) {
-            //$host = $_SERVER['HTTP_HOST'];
-            //$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+            $host = $_SERVER['HTTP_HOST'];
+            $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
             $extra = "Signin";
             unset($_SESSION["ID"]);
             unset($_SESSION['class']);
-            header("Location: https://localhost/MWIT-Laundry/$extra");
+            header("Location: https://$host$uri/$extra");
+            //header("Location: https://localhost/MWIT-Laundry/$extra");
         }
     }
 ?>
